e77d537031c6e240ff7b9b67356243e5949c53eb
Robert Szczepanski <rszczepanski@antmicro.com>
2024-04-04 13:34:07 +0200

    Fix controller test syntax
    Signed-off-by: Robert Szczepanski <rszczepanski@antmicro.com>

--- verification/block/i3c_ctrl/test_reset.py
+++ verification/block/i3c_ctrl/test_reset.py
@@ -5,6 +5,6 @@ import cocotb
 
 
 @cocotb.test()
-async def run_test():
+async def run_test(dut):
     """Run test for mixed signal simulation - automatic trimming of a voltage regulator."""
     pass
