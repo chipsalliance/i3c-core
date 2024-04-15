358375309209d611fdf54c631f9bb7f0a2463d3c
Wiktoria Kuna <wkuna@antmicro.com>
2024-04-15 15:41:36 +0200

    i3c_ahb_if: tests: WIP: Restructure & add new test
    Internal-tag: [#57847]
    Signed-off-by: Wiktoria Kuna <wkuna@antmicro.com>

--- verification/block/i3c_ahb_if/test_single_read_write_cmds.py
+++ verification/block/i3c_ahb_if/test_single_read_write_cmds.py
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-from asyncio import TimeoutError, wait_for
 from ctypes import Array
 from random import randint, randrange
 from typing import List, Tuple
@@ -91,111 +90,97 @@ def printable(data: List[int]):
     return int("".join(map(str, data)))
 
 
-# class AHBFIFOTestInterface:
-#     """
-#     This interface initializes appropriate cocotb AHB models and provides abstractions for
-#     common functionalities, such as read / write to CSR.
-#     """
+class AHBFIFOTestInterface:
+    """
+    This interface initializes appropriate cocotb AHB models and provides abstractions for
+    common functionalities, such as read / write to CSR.
+    """
 
-#     def __init__(self, dut: SimHandleBase):
-#         self.dut = dut
+    def __init__(self, dut: SimHandleBase):
+        self.dut = dut
 
-#         # FIFO AHB Frontend
-#         self.AHBSubordinate = DUTSubordinate(dut, bus_width=32)
+        # FIFO AHB Frontend
+        self.AHBSubordinate = DUTSubordinate(dut, bus_width=32)
 
-#         # Simulated AHB in control of dispatching commands
-#         self.AHBManager = SimSimpleManager(bus_width=32)
+        # Simulated AHB in control of dispatching commands
+        self.AHBManager = SimSimpleManager(bus_width=32)
 
-#         # Cocotb-ahb-specific construct for simulation purposes
-#         self.interconnect = SimInterconnect()
+        # Cocotb-ahb-specific construct for simulation purposes
+        self.interconnect = SimInterconnect()
 
-#         # Cocotb-ahb-specific construct for simulation purposes
-#         self.wrapper = InterconnectWrapper()
+        # Cocotb-ahb-specific construct for simulation purposes
+        self.wrapper = InterconnectWrapper()
 
-#     async def register_test_interfaces(self):
-#         # Clocks & resets
-#         # self.AHBSubordinate.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
-#         self.AHBManager.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
-#         self.interconnect.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
-#         self.wrapper.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
-#         # Interconnect setup
-#         self.interconnect.register_subordinate(self.AHBSubordinate)
-#         self.interconnect.register_manager(self.AHBManager)
-#         # Handled address space
-#         self.interconnect.register_manager_subordinate_addr(self.AHBManager, self.AHBSubordinate, 0x0, 0x4000)
-#         self.wrapper.register_interconnect(self.interconnect)
+    async def register_test_interfaces(self):
+        # Clocks & resets
+        # self.AHBSubordinate.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
+        self.AHBManager.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
+        self.interconnect.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
+        self.wrapper.register_clock(self.dut.hclk_i).register_reset(self.dut.hreset_n_i, True)
+        # Interconnect setup
+        self.interconnect.register_subordinate(self.AHBSubordinate)
+        self.interconnect.register_manager(self.AHBManager)
+        # Handled address space
+        self.interconnect.register_manager_subordinate_addr(self.AHBManager, self.AHBSubordinate, 0x0, 0x4000)
+        self.wrapper.register_interconnect(self.interconnect)
 
+        await cocotb.start(self.AHBManager.start())
+        await cocotb.start(self.wrapper.start())
+        await cocotb.start(setup_dut(self.dut, (10, "ns")))
 
-# Send a read request & await the response
-async def read_csr(manager, addr: int) -> int:
-    manager.read(addr, 4)
-    # TODO: Make await dependent on clock cycles
-    try:
-        await wait_for(manager.transfer_done(), timeout=5)
-    except Exception:
-        raise TimeoutError(f"read_csr @ {hex(addr)} unsuccessfull: Operation timeout exceeded.")
-    return manager.get_rsp(addr, 4)
+    # Send a read request & await the response
+    async def read_csr(self, addr: int) -> int:
+        self.AHBManager.read(addr, 4)
+        # TODO: Make await dependent on clock cycles; throw error with timeouts
+        await self.AHBManager.transfer_done()
+        return self.AHBManager.get_rsp(addr, 4)
 
-
-# Send a write request & await transfer to finish
-async def write_csr(manager, addr: int, data: int, strb: Array[bool]) -> None:
-    manager.write(addr, len(strb), data, strb)
-    # TODO: Make await dependent on clock cycles
-    try:
-        await wait_for(manager.transfer_done(), timeout=5)
-    except Exception:
-        raise TimeoutError(
-            f"write_csr {hex(data)} @ {hex(addr)} unsuccessfull: Operation timeout exceeded."
-        )
+    # Send a write request & await transfer to finish
+    async def write_csr(self, addr: int, data: int, strb: Array[bool] = [1, 1, 1, 1]) -> None:
+        self.AHBManager.write(addr, len(strb), data, strb)
+        # TODO: Make await dependent on clock cycles
+        await self.AHBManager.transfer_done()
 
 
 def compare_values(before: List[int], expected: List[int], after: List[int], addr: int):
+    print(f"Before {before}")
+    print(f"Expected {expected}")
+    print(f"After {after}")
     assert all(
         [expected[i] == after[i] for i in range(len(expected))]
     ), f"Word at {addr:#x} differs. Expected: {printable(expected)} Got: {printable(after)}"
 
 
 @cocotb.test()
+async def run_read_hci_version_csr(dut: SimHandleBase):
+    """Run test to read HCI version register."""
+
+    tb = AHBFIFOTestInterface(dut)
+    await tb.register_test_interfaces()
+
+    addr = 0x0
+    expected = 0x120
+
+    read_value = await tb.read_csr(addr)
+    compare_values(None, expected, read_value, addr)
+
+
+@cocotb.test()
 async def run_test(dut: SimHandleBase):
-    """Run test for mixed read / write CSR commands."""
+    """Run test for mixed random read / write CSR commands."""
 
-    # tb = AHBFIFOTestInterface(dut)
-    # await tb.register_test_interfaces()
+    tb = AHBFIFOTestInterface(dut)
+    await tb.register_test_interfaces()
 
-    # FIFO AHB Frontend
-    AHBSubordinate = DUTSubordinate(dut, bus_width=32)
-
-    # Simulated AHB in control of dispatching commands
-    AHBManager = SimSimpleManager(bus_width=32)
-    AHBManager.register_clock(dut.hclk_i).register_reset(dut.hreset_n_i, True)
-
-    # Cocotb-ahb-specific construct for simulation purposes
-    interconnect = SimInterconnect().register_subordinate(AHBSubordinate)
-    interconnect.register_clock(dut.hclk_i).register_reset(dut.hreset_n_i, True)
-    interconnect.register_manager(AHBManager)
-    interconnect.register_manager_subordinate_addr(AHBManager, AHBSubordinate, 0x0, 0x4000)
-
-    # Cocotb-ahb-specific construct for simulation purposes
-    wrapper = InterconnectWrapper()
-    wrapper.register_clock(dut.hclk_i).register_reset(dut.hreset_n_i, True)
-    wrapper.register_interconnect(interconnect)
-
-    await cocotb.start(AHBManager.start())
-    await cocotb.start(wrapper.start())
-    await cocotb.start(setup_dut(dut, (10, "ns")))
     # Reference each CSR in the address space in a random order
     # and a random value for write.
-    # test_sequence = gen_rand_seq()
+    test_sequence = gen_rand_seq()
 
-    await ClockCycles(dut.hclk_i, 10000)._wait()
-
-    # for cmd in test_sequence:
-    #     addr, val, strb = cmd.to_tuple()
-
-    #     value_before_write = await read_csr(AHBManager, addr)
-    #     await write_csr(AHBManager, addr, val, strb)
-    #     value_after_write = await read_csr(AHBManager, addr)
-
-    #     compare_values(value_before_write, val, value_after_write)
+    for cmd in test_sequence:
+        addr, val, strb = cmd.to_tuple()
+        value_before_write = await tb.read_csr(addr)
+        await tb.write_csr(addr, val, strb)
+        value_after_write = await tb.read_csr(addr)
+        compare_values(value_before_write, val, value_after_write, addr)
 
     # TODO: Compare the whole address space (in case adjacent bytes have been overwritten)
