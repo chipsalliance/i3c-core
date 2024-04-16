5245ac07b91a61f5f5c5dc9e6dc03c5fd8162905
Robert Szczepanski <rszczepanski@antmicro.com>
2024-04-16 12:44:33 +0200

    Add I2C + PHY wrapper to control I3C bus in Cocotb test
    Internal-tag: [#57962]
    Signed-off-by: Robert Szczepanski <rszczepanski@antmicro.com>

--- verification/block/i2c_phy_integration/test_mem_rw.py
+++ verification/block/i2c_phy_integration/test_mem_rw.py
@@ -6,30 +6,19 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 from cocotbext.i2c import I2cMemory
-from i2c import (
-    i2c_cmd,
-    i2c_mem_read,
-    i2c_mem_write,
-    init_i2c_controller_ports,
-    reset,
-)
+from i2c import i2c_cmd, i2c_mem_read, i2c_mem_write, init_i2c_controller_ports, reset
 
 
 @cocotb.test()
 async def run_test(dut):
     """
     Executes random read and writes while checking data on the I2C FSM.
-    Additionally runs the observer which checks states of PHY IO lines.
     """
     I2C_DEV_ADDR = 0x50
     I2C_REG_ADDR = 0x10
 
     # I2C target
-    I2cMemory(sda=dut.ctrl_sda_o, sda_o=dut.ctrl_sda_i, scl=dut.ctrl_scl_o, scl_o=dut.ctrl_scl_i)
-
-    # Mock PHY I3C bus pull-up
-    dut.i3c_sda_i.value = 1
-    dut.i3c_scl_i.value = 1
+    I2cMemory(sda=dut.i3c_sda_o, sda_o=dut.i3c_sda_i, scl=dut.i3c_scl_o, scl_o=dut.i3c_scl_i)
 
     # Drive constant DUT inputs
     init_i2c_controller_ports(dut)
@@ -37,8 +26,6 @@ async def run_test(dut):
     # Start clock
     clock = Clock(dut.clk_i, 0.5, units="us")
     cocotb.start_soon(clock.start())
-
-    cocotb.start_soon(run_bus_observer(dut))
 
     # Reset
     await reset(dut)
