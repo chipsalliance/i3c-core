d119a300a4c786afb5d727d451affab659e1d008
Wiktoria Kuna <wkuna@antmicro.com>
2024-05-29 11:51:09 +0200

    ver: utils: Add clog2 function
    Internal-tag: [#60109]
    Signed-off-by: Wiktoria Kuna <wkuna@antmicro.com>

--- verification/block/common/utils.py
+++ verification/block/common/utils.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
+from math import ceil, log2
+
 import cocotb
 from cocotb.triggers import ClockCycles, ReadOnly
 
@@ -16,3 +18,7 @@ async def check_delayed(clock, signal, expected, delay):
     assert (
         int(signal.value) == expected
     ), f"Incorrect value of signal {signal._name} at {time_ns} ns ({signal.value} vs {expected})"
+
+
+def clog2(val: int):
+    return ceil(log2(val))
