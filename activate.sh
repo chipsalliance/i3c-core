164520a830d5269b263171760cc03304a78cf831
Michal Czyz <mczyz@antmicro.com>
2024-06-28 15:55:12 +0200

    Add environment variables to activate script
    Signed-off-by: Michal Czyz <mczyz@antmicro.com>

--- activate.sh
+++ activate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Useful for running tests directly in verification/block
+export I3C_ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export CALIPTRA_ROOT=${I3C_ROOT_DIR}/third_party/caliptra-rtl
+
+# Pyenv
 PYTHON_VERSION=3.11.0
 VENV_NAME=i3c
 
@@ -7,4 +12,5 @@ pyenv install ${PYTHON_VERSION} --skip-existing
 pyenv virtualenv ${PYTHON_VERSION} ${VENV_NAME} || true
 pyenv shell ${VENV_NAME}
 python --version
+pip install --upgrade pip
 python -m pip install -r "$(pwd)"/requirements.txt
