27335370f397e2a5ccc7e1f6a30129da5ee101c5
Maciej Kurc <mkurc@antmicro.com>
2024-09-12 16:44:14 +0200

    Move width converters to own source files
    Internal-tag: [#65498]
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- src/ctrl/width_converter_8toN.sv
+++ src/ctrl/width_converter_8toN.sv
@@ -1,55 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
 
-// Bus width converter from N-bit to 8-bit
-module width_converter_Nto8 #(
-    parameter int unsigned Width = 32
-) (
-
-    input logic clk_i,
-    input logic rst_ni,
-
-    input  logic             in_valid_i,
-    output logic             in_ready_o,
-    input  logic [Width-1:0] in_data_i,
-
-    output logic       out_valid_o,
-    input  logic       out_ready_i,
-    output logic [7:0] out_data_o
-);
-
-  // Number of bytes of wider data bus
-  localparam int unsigned Bytes = (Width + 7) / 8;
-
-  // Byte counter
-  logic [$clog2(Bytes):0] bcnt;
-
-  always_ff @(posedge clk_i)
-    if (!rst_ni) bcnt <= 0;
-    else begin
-      if ((bcnt == 0) & in_valid_i & in_ready_o) bcnt <= Bytes;
-      else if ((bcnt != 0) & out_valid_o & out_ready_i) bcnt <= bcnt - 1;
-    end
-
-  // Valid / ready
-  assign in_ready_o  = (bcnt == 0);
-  assign out_valid_o = (bcnt != 0);
-
-  // Data register
-  logic [Width-1:0] sreg;
-
-  always_ff @(posedge clk_i)
-    if (!rst_ni) sreg <= {Width{1'b0}};
-    else begin
-      if ((bcnt == 0) & in_valid_i & in_ready_o) sreg <= in_data_i;
-      else if ((bcnt != 0) & out_valid_o & out_ready_i) sreg <= sreg << 8;
-    end
-
-  // Data output
-  assign out_data_o = sreg[Width-1-:8];
-
-endmodule
-
-
 // Bus width converter from 8-bit to N-bit
 module width_converter_8toN #(
     parameter int unsigned Width = 32
