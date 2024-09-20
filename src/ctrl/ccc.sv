68b79f9ed13cb4448f7b5a999da03a91435e652a
Krzysztof Obłonczek <koblonczek@antmicro.com>
2024-09-20 16:16:26 +0200

    Add initial support for RSTACT
    Internal-tag: [#65809]
    Signed-off-by: Krzysztof Obłonczek <koblonczek@antmicro.com>

--- src/ctrl/ccc.sv
+++ src/ctrl/ccc.sv
@@ -3,6 +3,7 @@
 module ccc
   import controller_pkg::*;
   import i3c_pkg::*;
+  import I3CCSR_pkg::*;
 (
     input logic clk_i,
     input logic rst_ni,
@@ -11,7 +12,7 @@ module ccc
     input logic [7:0] command_code_i,
     input logic command_code_valid_i,
 
-    input logic defining_byte_i,
+    input logic [7:0] defining_byte_i,
     input logic defining_byte_valid_i,
 
     //TODO: Establish correct size
@@ -22,22 +23,28 @@ module ccc
     output logic response_byte_o,
     output logic response_valid_o,
 
-    output logic is_in_hdr_mode_o
+    output logic is_in_hdr_mode_o,
+
+    input I3CCSR_pkg::I3CCSR__in_t hwif_in,
+    output I3CCSR_pkg::I3CCSR__out_t hwif_out
 );
 
   // Latch CCC data
   logic [7:0] command_code;
   logic [7:0] defining_byte;
+  logic       defining_byte_valid;
   logic [7:0] command_data;
 
   always_ff @(posedge clk_i or negedge rst_ni) begin : proc_latch_inputs
     if (~rst_ni) begin
       command_code  <= '0;
       defining_byte <= '0;
+      defining_byte_valid <= '0;
       command_data  <= '0;
     end else begin
       command_code  <= command_code_valid_i ? command_code_i : command_code;
       defining_byte <= defining_byte_valid_i ? defining_byte_i : defining_byte;
+      defining_byte_valid <= defining_byte_valid_i;
       command_data  <= command_data_valid_i ? command_data_i : command_data;
     end
   end
@@ -47,6 +54,9 @@ module ccc
   logic [7:0] rx_data_buffer_size;
   logic [7:0] ibi_status_size;
   logic [7:0] cr_queue_size;
+
+  logic [7:0] rstact_rst_action_next;
+  logic       rstact_rst_action_valid;
 
   assign tx_data_buffer_size = queue_size_reg_i[31:24];
   assign rx_data_buffer_size = queue_size_reg_i[23:16];
@@ -58,6 +68,8 @@ module ccc
 
   always_comb begin
     response_valid_o = '0;
+    rstact_rst_action_next = '0;
+    rstact_rst_action_valid = '0;
     unique case (command_code)
       // Idle: Wait for command appearance in the Command Queue
       `I3C_DIRECT_GETMRL: begin
@@ -71,12 +83,28 @@ module ccc
       `I3C_BCAST_ENTHDR0: begin
         is_in_hdr_mode_o = '1;
       end
+      `I3C_DIRECT_RSTACT, `I3C_BCAST_RSTACT: begin
+        unique case (defining_byte)
+          `I3C_RSTACT_NO_RESET, `I3C_RSTACT_PERIPHERAL_RESET: begin
+            if (defining_byte_valid) begin
+              rstact_rst_action_next = defining_byte;
+              rstact_rst_action_valid = '1;
+            end
+          end
+        endcase
+      end
       default: begin
         response_byte_o  = '0;
         response_valid_o = '0;
         is_in_hdr_mode_o = '0;
       end
     endcase
+  end
+
+  always_ff @(posedge clk_i) begin
+    if (rstact_rst_action_valid) begin
+      hwif_in.I3C_EC.StdbyCtrlMode.STBY_CR_CCC_CONFIG_RSTACT_PARAMS.RST_ACTION.next <= rstact_rst_action_next;
+    end
   end
 
 endmodule
