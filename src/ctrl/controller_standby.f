ee0c82e7b13ece48e17f7d03c59297d2dcba338c
Robert Szczepanski <rszczepanski@antmicro.com>
2024-12-17 12:09:43 +0100

    Add filelist for I3C components
    Signed-off-by: Robert Szczepanski <rszczepanski@antmicro.com>
    Co-Authored-By: Michal Czyz <mczyz@antmicro.com>

---
+++ src/ctrl/controller_standby.f
@@ -0,0 +1,23 @@
++incdir+${I3C_ROOT_DIR}/src
+${I3C_ROOT_DIR}/src/ctrl/controller_pkg.sv
+${I3C_ROOT_DIR}/src/i3c_pkg.sv
+// ${I3C_ROOT_DIR}/src/ctrl/ibi.sv
+${I3C_ROOT_DIR}/src/ctrl/bus_tx.sv
+${I3C_ROOT_DIR}/src/ctrl/bus_tx_flow.sv
+${I3C_ROOT_DIR}/src/ctrl/bus_rx_flow.sv
+${I3C_ROOT_DIR}/src/ctrl/bus_timers.sv
+${I3C_ROOT_DIR}/src/ctrl/target_reset_detector.sv
+${I3C_ROOT_DIR}/src/ctrl/stable_high_detector.sv
+${I3C_ROOT_DIR}/src/ctrl/bus_monitor.sv
+${I3C_ROOT_DIR}/src/ctrl/ccc.sv
+${I3C_ROOT_DIR}/src/ctrl/addr_match.sv
+${I3C_ROOT_DIR}/src/ctrl/edge_detector.sv
+${I3C_ROOT_DIR}/src/ctrl/descriptor_rx.sv
+${I3C_ROOT_DIR}/src/ctrl/descriptor_tx.sv
+${I3C_ROOT_DIR}/src/ctrl/descriptor_ibi.sv
+${I3C_ROOT_DIR}/src/ctrl/i2c_target_fsm.sv
+${I3C_ROOT_DIR}/src/ctrl/i3c_target_fsm.sv
+${I3C_ROOT_DIR}/src/ctrl/flow_standby_i2c.sv
+${I3C_ROOT_DIR}/src/ctrl/controller_standby_i2c.sv
+${I3C_ROOT_DIR}/src/ctrl/controller_standby_i3c.sv
+${I3C_ROOT_DIR}/src/ctrl/controller_standby.sv
---
+++ src/ctrl/i3c_target_fsm.f
@@ -0,0 +1,4 @@
++incdir+${I3C_ROOT_DIR}/src
+${I3C_ROOT_DIR}/src/ctrl/controller_pkg.sv
+${I3C_ROOT_DIR}/src/i3c_pkg.sv
+${I3C_ROOT_DIR}/src/ctrl/i3c_target_fsm.sv
