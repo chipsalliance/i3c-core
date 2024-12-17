7c94cb5c17a94614d89c9af8460a6885d6010cd2
Jan Olencki <jolencki@antmicro.com>
2024-12-17 14:58:34 +0000

    Remove duplicated file with a package from Makefile

--- verification/cocotb/common.mk
+++ verification/cocotb/common.mk
@@ -17,7 +17,6 @@ export PYTHONPATH := $(PYTHONPATH):$(CURDIR)/common
 # Common sources
 COMMON_SOURCES  = \
     $(CALIPTRA_ROOT)/src/caliptra_prim/rtl/caliptra_prim_assert.sv \
-    $(CALIPTRA_ROOT)/src/caliptra_prim/rtl/caliptra_prim_pkg.sv \
     $(CALIPTRA_ROOT)/src/caliptra_prim_generic/rtl/caliptra_prim_generic_flop.sv \
     $(CALIPTRA_ROOT)/src/caliptra_prim/rtl/caliptra_prim_flop.sv \
     $(CALIPTRA_ROOT)/src/caliptra_prim/rtl/caliptra_prim_flop_2sync.sv
