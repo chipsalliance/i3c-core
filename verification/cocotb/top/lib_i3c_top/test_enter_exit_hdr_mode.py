20957a56a9cd7544fdb6715ee540efbeeba7626a
Robert Szczepanski <rszczepanski@antmicro.com>
2024-12-18 13:49:08 +0100

    Fix syntax error in HDR mode test
    Signed-off-by: Robert Szczepanski <rszczepanski@antmicro.com>

--- verification/cocotb/top/lib_i3c_top/test_enter_exit_hdr_mode.py
+++ verification/cocotb/top/lib_i3c_top/test_enter_exit_hdr_mode.py
@@ -42,13 +42,13 @@ async def test_enter_exit_hdr_mode(dut):
     await i3c_controller.i3c_ccc_write(ENTHDR0, broadcast_data=[])
 
     assert (
-        dut.xi3c_wrapper.i3c.xcontroller.xcontroller_standby.xcontroller_standby_i3c.xi3c_target_fsm.state_d
+        dut.xi3c_wrapper.i3c.xcontroller.xcontroller_standby.xcontroller_standby_i3c.u_i3c_target_fsm.state_d
         == 0xA0
     )  # IdleHDR
 
     await i3c_controller.send_hdr_exit()
 
     assert (
-        dut.xi3c_wrapper.i3c.xcontroller.xcontroller_standby.xcontroller_standby_i3c.xi3c_target_fsm.state_d
+        dut.xi3c_wrapper.i3c.xcontroller.xcontroller_standby.xcontroller_standby_i3c.u_i3c_target_fsm.state_d
         == 0
     )  # Idle
