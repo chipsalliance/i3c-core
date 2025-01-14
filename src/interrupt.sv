687c4aff93bf08d0ba7d76c1a962b2cfba8a8e52
Maciej Kurc <mkurc@antmicro.com>
2025-01-14 10:57:15 +0100

    Add trigger interrupts on events (edges) instead of states (levels)
    Internal-tag: [#71469]
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- src/interrupt.sv
+++ src/interrupt.sv
@@ -1,7 +1,12 @@
 /*
 Single interrupt request handling logic
 */
-module interrupt (
+module interrupt # (
+    parameter bit Edge = 1      // 1 - trigger on rising edge, 0 - trigger on high level
+) (
+    input  logic clk_i,
+    input  logic rst_ni,
+
     input  logic irq_i,         // IRQ input from source
     input  logic irq_force_i,   // Force IRQ input
 
@@ -17,16 +22,37 @@ module interrupt (
     output logic irq_o          // IRQ output
 );
 
-    // Interrupt request (masked)
     logic  irq;
-    assign irq = (irq_i | irq_force_i) & sts_ena_i;
+    logic  trg;
 
-    // Status storage
+    // Interrupt request input
+    assign irq = (irq_i | irq_force_i);
+
+    // Trigger on edge
+    generate if (Edge == 1'b1) begin
+        logic irq_r;
+        always_ff @(posedge clk_i or negedge rst_ni) begin
+            if (!rst_ni) begin
+                irq_r <= '0;
+                trg   <= '0;
+            end else begin
+                irq_r <= irq;
+                trg   <= irq & !irq_r;
+            end
+        end
+
+    // Trigger on level
+    end else begin
+        assign trg = irq;
+
+    end endgenerate
+
+    // Status storage control
     always_comb begin
         sts_o       = '0;
         sts_we_o    = '0;
 
-        if (irq) begin
+        if (trg & sts_ena_i) begin
             sts_o       = '1;
             sts_we_o    = '1;
         end else if (clr_i) begin
