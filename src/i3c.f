c4e33bb465a525ef55862b0549667b0c7104ba28
Wiktoria Kuna <wkuna@antmicro.com>
2025-03-19 14:19:37 +0100

    Follow up: Use upstream AXI module
    Signed-off-by: Wiktoria Kuna <wkuna@antmicro.com>

--- src/i3c.f
+++ src/i3c.f
@@ -1,8 +1,8 @@
 +incdir+${CALIPTRA_ROOT}/src/libs/rtl
 +incdir+${CALIPTRA_ROOT}/src/caliptra_prim/rtl
++incdir+${CALIPTRA_ROOT}/src/axi/rtl
 +incdir+${I3C_ROOT_DIR}/src
 +incdir+${I3C_ROOT_DIR}/src/libs
-+incdir+${I3C_ROOT_DIR}/src/libs/axi
 ${CALIPTRA_ROOT}/src/caliptra_prim/rtl/caliptra_prim_pkg.sv
 ${CALIPTRA_ROOT}/src/caliptra_prim/rtl/caliptra_prim_util_pkg.sv
 ${CALIPTRA_ROOT}/src/caliptra_prim/rtl/caliptra_prim_count_pkg.sv
