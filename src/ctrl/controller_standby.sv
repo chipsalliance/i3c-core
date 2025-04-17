c49a47012cc65f2c3de31197e2c4b8e4d2f02578
Karol Gugala <kgugala@antmicro.com>
2025-04-17 14:55:32 +0200

    wire up entdaa_ccc module

--- src/ctrl/controller_standby.sv
+++ src/ctrl/controller_standby.sv
@@ -381,7 +381,7 @@ module controller_standby
   ) xcontroller_standby_i3c (
       .clk_i(clk_i),
       .rst_ni(rst_ni),
-      .id_i(id_i)
+      .id_i(id_i),
       .ctrl_bus_i(ctrl_bus_i[1]),
       .ctrl_scl_o(ctrl_scl_o[1]),
       .ctrl_sda_o(ctrl_sda_o[1]),
