64cc6b8eddd06f492708711b11c72862fba59b0a
Miguel Osorio <miguelosorio@google.com>
2026-04-02 21:35:27 +0000

    [env] update requirements.txt and setenv.sh
    Updated requirements.txt to python-constraint==1.3.1.
    
    Added CALIPTRA_PRIM_MODULE_PREFIX and CALIPTRA_PRIM_ROOT exports to setenv.sh.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- setenv.sh
+++ setenv.sh
@@ -9,3 +9,6 @@ fi
 # Useful for running tests directly in verification/block
 export I3C_ROOT_DIR=$( cd "$( dirname "$script_path" )" &> /dev/null && pwd )
 export CALIPTRA_ROOT=${I3C_ROOT_DIR}/third_party/caliptra-rtl
+
+export CALIPTRA_PRIM_MODULE_PREFIX=caliptra_prim_generic
+export CALIPTRA_PRIM_ROOT=${CALIPTRA_ROOT}/src/caliptra_prim_generic
